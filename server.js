9e69d9cf54e0c6e5209531c946ea3a257e561bc9
sarika-sukalpa <sarikasukalpatechsolutions@gmail.com>
2026-08-24 13:32:07 +0530

    added new bus components

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require('body-parser');
 const path = require('path'); 
 const cors = require("cors")
-const port = 3007;
+const port = 3009;
 const { getMongoConnection } = require("./controllers/mongoService")
 const { registerRoutes } = require("./routes/routesRegistry")
 
